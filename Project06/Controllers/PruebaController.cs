370e098dd55909efa711aa14380f1b10d860f1d9
Andrey <pirlocc@gmail.com>
2021-10-18 12:10:57 -0600

    Cambio prueba

---
+++ Project06/Controllers/PruebaController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Project06.Controllers
+{
+    public class PruebaController : Controller
+    {
+        //Prueba
+        // GET: Prueba
+        public ActionResult Index()
+        {
+            return View();
+
+        }
+    }
+}
