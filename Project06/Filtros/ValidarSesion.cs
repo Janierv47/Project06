ec4a9de9d2b1efeaab64d794d7fd4d86eeab0459
Andrey <pirlocc@gmail.com>
2021-11-27 16:45:19 -0600

    B

---
+++ Project06/Filtros/ValidarSesion.cs
@@ -0,0 +1,26 @@
+﻿using Project06.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Project06.Filtros
+{
+    public class ValidarSesionFilter : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+
+            var sesion = (AccesoModel)HttpContext.Current.Session["Usuario"];
+
+
+            if (sesion == null)
+            {
+                filterContext.HttpContext.Response.Redirect("~/InicioSesion/Autenticacion");
+            }
+
+            base.OnActionExecuting(filterContext);
+        }
+    }
+}
